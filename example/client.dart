532cb51dc7c47382fefb6bbfac49716a59dd003c
Nate Bosch <nbosch@google.com>
2021-11-09 14:37:36 -0800

    No need for extra directory hopping - use resolvedExecutable directly

--- example/client.dart
+++ example/client.dart
@@ -3,13 +3,11 @@ import 'dart:io';
 import 'package:bazel_worker/driver.dart';
 import 'package:path/path.dart' as p;
 
-final sdkDir = p.dirname(p.dirname(Platform.resolvedExecutable));
-
 void main() async {
   var scratchSpace = await Directory.systemTemp.createTemp();
   var driver = BazelWorkerDriver(
       () => Process.start(
-          p.join(sdkDir, 'bin', 'dart'), [p.join('example', 'worker.dart')],
+          Platform.resolvedExecutable, [p.join('example', 'worker.dart')],
           workingDirectory: scratchSpace.path),
       maxWorkers: 4);
   driver.doWork(WorkRequest()..arguments.add('foo'));
