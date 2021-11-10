f0d526c0a4b71ef63859aaeeb4107e7a9c2d65b9
Nate Bosch <nbosch@google.com>
2021-11-09 16:08:21 -0800

    Tweaks
    - Remove dependency on `path` by resolving with Uri.
    - Add an output demonstrating how the work result can be communicated
      back through a file.
    - Add a README with run instructions.

--- example/client.dart
+++ example/client.dart
@@ -1,18 +1,22 @@
 import 'dart:io';
 
 import 'package:bazel_worker/driver.dart';
-import 'package:path/path.dart' as p;
 
 void main() async {
   var scratchSpace = await Directory.systemTemp.createTemp();
   var driver = BazelWorkerDriver(
       () => Process.start(Platform.resolvedExecutable,
-          [p.join(p.absolute(p.current), 'example', 'worker.dart')],
+          [Platform.script.resolve('worker.dart').toFilePath()],
           workingDirectory: scratchSpace.path),
       maxWorkers: 4);
   var response = await driver.doWork(WorkRequest()..arguments.add('foo'));
   if (response.exitCode != EXIT_CODE_OK) {
     print('Worker request failed');
+  } else {
+    print('Worker request succeeded, file content:');
+    var outputFile = File.fromUri(scratchSpace.uri.resolve('hello.txt'));
+    print(await outputFile.readAsString());
   }
+  await scratchSpace.delete(recursive: true);
   await driver.terminateWorkers();
 }
